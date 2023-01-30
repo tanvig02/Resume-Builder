004c39617627664ce76b2aac7cb31a1c81ceca5b
Tanvi Gaikwad <tanvigaikwad2002@gmail.com>
2023-01-30 13:50:10 +0530

    package json file

--- server/app.js
+++ server/app.js
@@ -29,6 +29,7 @@ app.use(require("./router/asynAuth"));
 //   res.send("about page");
 // });
 
+app.use(express.static(path.join(__dirname, "./package.json")));
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
